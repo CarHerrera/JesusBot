2a376ef587bfba2e529abf2ef9c1595253c39a2c
Carlos <herrera4345@gmail.com>
2023-01-22 03:43:52 -0500

    Added check to see if Jesus is the user of the echo command or not.

--- commands/echo.js
+++ commands/echo.js
@@ -16,6 +16,9 @@ module.exports = {
                 .setDescription('If you want said shit to be public.')
                 .setRequired(true)),
 	async execute(interaction) {
+        if(interaction.member.id != '213090776001937409') {
+            interaction.reply('Hey you\'re not the CEO of Jesus. You\'re not allowed to use this command.')
+        }
         const phrase = interaction.options.getString('echo');
         const vis = interaction.options.getBoolean('ephemeral');
         if(vis === true){
